81bb66be6e872051e30db33637250c6761e821c6
Pardhu Chowdary <pardhu@iitk.ac.in>
2016-12-25 11:12:04 +0530

    Intial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>index</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	
+	
+	<body>
+		<div style="height: 38px">This is a clearance area for the nav bar </div>
+		<div id="wrapper">
+			<div class="errorbox"></div>
+			<div class="msgbox"></div>
+			<h3>
+				Please enter your login credentials
+			</h3>
+			<!-- TODO Fill the attributes-->
+			<form action="" method="post">
+				<div class="form labels">
+					Roll no:<br>
+					Password:
+				</div>
+				<div class="form inputs">
+					<input type="text" name="username"><br>
+					<input type="password" name="password">
+				</div>
+				<input id="submit" type="submit" value="login">
+			</form>
+		</div>
+		<nav>
+			<a href="#" class="nav_item">Home</a>
+			<a href="#" class="nav_item">About us</a>
+			<a href="#" class="nav_item">Contact us</a>
+			<a href="#" class="nav_item">Another nav item</a>
+		</nav>
+	</body>
+</html>
